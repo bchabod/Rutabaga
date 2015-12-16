1d83c2f38a251fe1d13e1ebe636c8d0ab56a04a5
bchabod <be.chabod@gmail.com>
2015-12-16 08:33:25 +0100

    Major refactoring - rduteil's project applied

---
+++ app/src/main/java/com/h4104/rutabaga/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.h4104.rutabaga;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+
+public class SplashScreen extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this, MapsActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
